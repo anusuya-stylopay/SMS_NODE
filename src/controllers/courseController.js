58762ec45ed644e868a78fb7b977cd5e9e1da857
anusuya-stylopay <anusuya@stylopay.com>
2024-11-28 12:19:19 +0530

    courseRoutes & UniversityRoutes added

--- src/controllers/courseController.js
+++ src/controllers/courseController.js
@@ -9,8 +9,11 @@ const getCourse = async (req, res) => {
         'Authorization': `Zoho-oauthtoken ${token.access_token}`,
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36'
     };
+    const { page } = req.query;
     const params = {
         fields: 'Name,Course_URL,University,Duration,Department,Educational_Qualification,Creator_ID,Course_Fee,Course_Level,Country,Course_Description,Requirement,Course_Type,English_Requirement,Assigned_Counsellor,University_Name,Intake,Eligibility',
+        per_page: 5,
+        page
     };
 
     try {
@@ -26,38 +29,20 @@ const filterCourse = async (req, res) => {
     const url = 'https://api.sandbox.edbucket.com/crm/api/v1/Course/search';
     const _headers = {
         'Authorization': `Zoho-oauthtoken ${token.access_token}`,
-        'User-Agent': 'Mozilla/5.0 Mozilla/5.0 (platform; rv:geckoversion) Gecko/geckotrail Firefox/firefoxversion'
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36'
     };
     const { criteria, page } = req.query;
 
     const _params = {
         fields: 'Name,Course_URL,University,Duration,Department,Educational_Qualification,Creator_ID,Country,Subject,Assigned_Counsellor',
         per_page: 15,
-        page: page,
-        criteria: criteria
+        page,
+        criteria
     };
 
     try {
         const response = await axios.get(url, { params: _params, headers: _headers })
         res.send(response.data);
-        // const data = response.data.id
-        // const apiUrl = `${(res.fetchDNSDataResult).baseUrl}/crm/api/v1/Course`;
-        // const headers = {
-        //     'Authorization': `Zoho-oauthtoken ${token}`,
-        //     'User-Agent': 'Mozilla/5.0 Mozilla/5.0 (platform; rv:geckoversion) Gecko/geckotrail Firefox/firefoxversion'
-        // };
-        // const params = {
-        //     fields: 'Name,Course_URL,University,Duration,Department,Educational_Qualification,Creator_ID,Course_Fee,Course_Level,Country,Course_Description,Course_level,Requirement',
-        //     criteria: `(University.id:equals:${data})`
-        // };
-        // try {
-        //     const response = await axios.get(apiUrl, { params: params, headers: headers })
-        //     res.send(response.data);
-        // }
-
-        // catch (error) {
-        //     res.status(error.response ? error.response.status : 500).send(error.message);
-        // }
     }
     catch (error) {
         res.status(error.response ? error.response.status : 500).send(error.message);
--- src/controllers/universityController.js
+++ src/controllers/universityController.js
@@ -29,15 +29,18 @@ const getUniversities = async (req, res) => {
 const getFilteredUniversities = async (req, res) => {
     const token = await getToken();
     console.log("token :",token)
-    const apiUrl = 'https://api.sandbox.edbucket.com/crm/api/v1/University/search?criteria=(Country:equals:India)';
+    const apiUrl = 'https://api.sandbox.edbucket.com/crm/api/v1/University/search';
     console.log("universityAPI_URL :",apiUrl)
     const headers = {
         'Authorization': `Zoho-oauthtoken ${token.access_token}`,
-        'User-Agent': 'Mozilla/5.0 Mozilla/5.0 (platform; rv:geckoversion) Gecko/geckotrail Firefox/firefoxversion'
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36'
     };
+    const { criteria, page } = req.query;
     const params = {
         fields: 'Name,Owner,creatorID,Country,Modified_Time,Requirement,University_Description,Website_Link,Assigned_Counsellor,Course_List',
-         criteria: `(University.id:equals:${data})`
+        criteria,
+        per_page: 15,
+        page
     };
     try {
         const response = await axios.get(apiUrl, { params: params, headers: headers })
